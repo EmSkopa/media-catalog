9c8533e16fab92922edbb8de62a120ac3a835343
Emir Skopljak <emir.skopljo@gmail.com>
2024-08-30 01:58:28 +0200

    Prepared for the interview

--- src/main/java/com/example/demo/mediaPublish/user/config/AuthServerConfiguration.java
+++ src/main/java/com/example/demo/mediaPublish/user/config/AuthServerConfiguration.java
@@ -1,48 +1,11 @@
 package com.example.demo.mediaPublish.user.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class AuthServerConfiguration extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer {
+public class AuthServerConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-
-        return super.authenticationManagerBean();
-    }
-
-    @Autowired
-    AuthenticationManager authManager;
-
-    private PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-
-    }
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer client) throws Exception {
-
-        client.inMemory().withClient("emir").secret(passwordEncoder.encode("emirpass")).scopes("READ", "WRITE").authorizedGrantTypes("password", "authorization_code");
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoint) throws Exception {
-
-        endpoint.authenticationManager(authManager);
-    }
 }
